10339d023acfaf0c4e065af9d0b049155a30565f
Fabián Novoa <fabiansteven90@gmail.com>
2017-05-17 17:04:43 -0500

    Definir la entidad Producto que contenga 3 atributos (nombre, marca y precio).

---
+++ src/main/scala/co/s4n/capacitaciones/sesion1/HighOrderFunctions/Producto.scala
@@ -0,0 +1,9 @@
+package co.s4n.capacitaciones.sesion1.HighOrderFunctions
+
+/**
+  * Created by fnovoa10 on 05/17/17.
+  */
+class Producto(
+                nombre: String,
+                precio: Int,
+                marca: String) {}
