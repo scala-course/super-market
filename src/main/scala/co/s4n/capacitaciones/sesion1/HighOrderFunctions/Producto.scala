738997d4709a74f341fd023d38eabf546fb7afd3
Fabián Novoa <fabiansteven90@gmail.com>
2017-05-22 13:45:18 -0500

    Align product

--- src/main/scala/co/s4n/capacitaciones/sesion1/HighOrderFunctions/Producto.scala
+++ src/main/scala/co/s4n/capacitaciones/sesion1/HighOrderFunctions/Producto.scala
@@ -1,11 +1,11 @@
 package co.s4n.capacitaciones.sesion1.HighOrderFunctions
 
 /**
- * Created by fnovoa10 on 05/17/17.
- */
+  * Created by fnovoa10 on 05/17/17.
+  */
 
 case class Producto(var nombre: String,
-    var marca: String,
-    var precio: Double) {
+                    var marca: String,
+                    var precio: Double) {
 
 }
